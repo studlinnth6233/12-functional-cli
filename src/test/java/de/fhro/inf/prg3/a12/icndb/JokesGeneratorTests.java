047df7c80e35ac21bd03331f2050b31e94134b87
Peter Kurfer <peter.kurfer@fh-rosenheim.de>
2017-12-28 20:58:15 +0100

    Updated code spec

--- src/test/java/de/fhro/inf/prg3/a12/icndb/JokesGeneratorTests.java
+++ src/test/java/de/fhro/inf/prg3/a12/icndb/JokesGeneratorTests.java
@@ -22,31 +22,13 @@ class JokesGeneratorTests {
     @Test
     void testRandomStream() {
         /* timeout to ensure that stream does not loop forever */
-        assertTimeout(Duration.ofSeconds(5L), () -> {
-            Stream<ResponseWrapper<JokeDto>> randomJokesStream = jokeGenerator.randomJokesStream();
-
-            randomJokesStream
-                    .filter(Objects::nonNull)
-                    .skip(3)
-                    .limit(5)
-                    .map(ResponseWrapper::getValue)
-                    .map(JokeDto::getJoke)
-                    .forEach(Assertions::assertNotNull);
-        });
+        /* TODO implement a test for the random joke stream */
     }
 
 
     @Test
     void testJokesStream() {
-        assertTimeout(Duration.ofMinutes(1L), () -> {
-            jokeGenerator.jokesStream()
-                    .filter(Objects::nonNull)
-                    .map(ResponseWrapper::getValue)
-                    .skip(200L)
-                    .limit(400L)
-                    .map(JokeDto::getJoke)
-                    .forEach(Assertions::assertNotNull);
-        });
+        /* TODO implement a test for the linear jokes generator */
     }
 
 }
